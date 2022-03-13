7f909faf056f14579377128972802aefa64eb2cd
Victor Maslov <nakilon@gmail.com>
2022-03-13 17:55:46 +0300

    sorting by alexa rank; added 3 more resources

--- main.rb
+++ main.rb
@@ -6,16 +6,17 @@ YAML.load(yaml).each do |region, resources|
   puts "| название | уровень<br>свидомости | пруфы |"
   puts "| -------- | --------------------- | ----- |"
   puts( resources.map do |req, opt|
-    domain, banned, alexa, name = req.split(" ", 4)
+    link, banned, alexa, name = req.split(" ", 4)
     _ = opt.fetch(:links, []).map{ |_| _.split(" ", 4).tap{ |_| _[3] = _[3] } }.transpose
     _ = [[0], [], [], []] if _.empty?
-    "| [#{name}](#{domain})#{
-      "<br>#{opt[:comment]}" if opt.key? :comment
+    domain = link[/[^\/]+\z/]
+    ["| #{name ? "[#{name}](#{link}) #{domain.sub /./, '\0<area>'}" : "[#{domain}](#{link})"}#{
+      " (#{opt[:comment]})" if opt.key? :comment
     }#{
       "<br>#{opt[:chefs].gsub ", ", "<br>"}" if opt.key? :chefs
     } | #{_[0].max} | #{
       _[1].map.with_index{ |_, i| "[[#{i + 1}]](#{_})" }.join(", ")
-    } |"
-  end.sort )
+    } |", alexa.to_i]
+  end.sort_by(&:last).map(&:first) )
   puts ""
 end
