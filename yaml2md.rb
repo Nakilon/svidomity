61df679923cac43863072402dd97bfa76f08ae27
Victor Maslov <nakilon@gmail.com>
2022-06-01 13:17:35 +0300

    vk

--- yaml2md.rb
+++ yaml2md.rb
@@ -1,22 +1,46 @@
 require "yaml"
-puts ARGF.gets
-YAML.load(ARGF.read).each do |region, resources|
+puts "# Специализированный навигатор по украинским СМИ"
+
+# 1 - когда просто недостоверная информация без злого умысла
+# 2 - намеренное разжигаение, фейки и особая лексика
+
+YAML.load(File.read "websites.yaml").each do |region, resources|
   puts "\n## #{region}"
-  puts "| название | уровень<br>свидомости | статьи и их авторы | заблокирован<br>в РФ |"
-  puts "| -------- | :-------------------: | ------------------ | :------------------: |"
+  puts "| название | уровень<br>свидомости | посты и<br>их авторы | заблокирован<br>в РФ |"
+  puts "| -------- | :-------------------: | -------------------- | :------------------: |"
   puts( resources.map do |req, opt|
     link, alexa, name = req.split(" ", 3)
-    _ = opt.fetch(:links, []).map{ |_| _.split(" ", 4).tap{ |_| _[3] = _[3] } }.transpose
-    _ = [[0], [], [], []] if _.empty?
+    links = opt.fetch(:links, []).map{ |_| _.split(" ", 4).tap{ |_| _[3] = _[3] } }.transpose
+    links = [[0], [], [], []] if links.empty?
     domain = link[/[^\/]+\z/]
-    ["#{name ? "[#{name}](#{link}) #{domain.sub /./, '\0<area>'}" : "[#{domain}](#{link})"}#{
+    ["#{name ? "[#{name}](#{link}) #{domain.sub /./, '\0<area>'}" : "[#{domain}](#{link})"}#{  # IIRC <area> is a hack against autolinking on Github
       " (#{opt[:comment]})" if opt.key? :comment
     }#{
-      "<br>#{opt[:chefs].gsub ", ", "<br>"}" if opt.key? :chefs
-    } | #{_[0].max} | #{
-      _[1].zip(_[2]).sort_by(&:last).map.with_index{ |(__,*), i| "[[#{i + 1}]](#{__})" }.join(", ")
+      "<br>#{opt[:chiefs].gsub ", ", "<br>"}" if opt.key? :chiefs
+    } | #{links[0].max} | #{
+      links[1].zip(links[2]).sort_by(&:last).map.with_index{ |(__,*), i| "[[#{i + 1}]](#{__})" }.join(", ")
     }#{
-      _[3].compact.uniq.sort.map{ |_| "<br>#{_}" }.join
+      links[3].compact.uniq.sort.map{ |_| "<br>#{_}" }.join
     } | #{opt[:banned] ? "да" : "нет"}", alexa.to_i]
+  end.sort_by(&:last).map(&:first) )
+end
+
+YAML.load(File.read "vk.yaml").each do |region, resources|
+  puts "\n## #{region}"
+  puts "| название | уровень<br>свидомости | посты и<br>их авторы |"
+  puts "| -------- | :-------------------: | -------------------- |"
+  puts( resources.map do |req, opt|
+    link, subs, name = req.split(" ", 3)
+    links = opt.fetch(:links, []).map{ |_| _.split(" ", 4).tap{ |_| _[3] = _[3] } }.transpose
+    links = [[0], [], [], []] if links.empty?
+    ["[#{name}](#{"https://vk.com/#{link}"}) #{link}#{
+      " (#{opt[:comment]})" if opt.key? :comment
+    }#{
+      "<br>#{opt[:chiefs].gsub ", ", "<br>"}" if opt.key? :chiefs
+    } | #{links[0].max} | #{
+      links[1].zip(links[2]).sort_by(&:last).map.with_index{ |(__,*), i| "[[#{i + 1}]](#{__})" }.join(", ")
+    }#{
+      links[3].compact.uniq.sort.map{ |_| "<br>#{_}" }.join
+    }", -subs.to_i.to_i]
   end.sort_by(&:last).map(&:first) )
 end
