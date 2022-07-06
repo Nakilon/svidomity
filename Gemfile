f82c7269108ce627d1c299f76018f68c603ac6ab
Victor Maslov <nakilon@gmail.com>
2022-07-06 10:22:34 +0300

    colorized first column

--- Gemfile
+++ Gemfile
@@ -1,2 +1,4 @@
 source "https://rubygems.org"
 gem "commonmarker"
+gem "tablestructured"
+gem "oga"
--- md2htm.rb
+++ md2htm.rb
@@ -1,4 +1,5 @@
 require "commonmarker"
+require "oga"
 puts <<~HEREDOC
   <!DOCTYPE html>
   <html>
@@ -8,7 +9,22 @@ puts <<~HEREDOC
       <style>#{File.read "markdown.css"}</style>
     </head>
     <body class="markdown-body Box-body">
-      #{CommonMarker.render_html ARGF.read, %i{ DEFAULT UNSAFE }, %i{ table }}
+      #{
+        Oga.parse_html(CommonMarker.render_html ARGF.read, %i{ DEFAULT UNSAFE }, %i{ table }).tap do |html|
+          html.css("table").each do |_|
+            TableStructured.new(_).each do |_|
+              _.values.first.set "style", (
+                case _.уровеньсвидомости.text
+                when ?2 ; "background-color: #ffeeee"
+                when ?1 ; "background-color: #ffffee"
+                when ?0 ; "background-color: #eeffee"
+                else ; fail "invalid уровень свидомости #{_.уровеньсвидомости.text.inspect}"
+                end
+              )
+            end
+          end
+        end
+      }
     </body>
   </html>
 HEREDOC
